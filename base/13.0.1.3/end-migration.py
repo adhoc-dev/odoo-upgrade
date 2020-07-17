6152286470080b619bf0024f1ab9feff6354c842
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-07-16 23:21:55 -0300

    [REF] disable reload translations

--- base/13.0.1.3/end-migration.py
+++ base/13.0.1.3/end-migration.py
@@ -16,5 +16,5 @@ def migrate(env, version):
         for filename in ['data/mis_report.xml', 'data/mis_report_instance.xml', 'data/mis_report_style.xml']:
             openupgrade.load_data(env.cr, 'mis_builder_cash_flow', filename)
 
-    # recargamos traducciones (por ahora no lo hacemos, si llegamos a ver que suma y es necesario probamos activarlo)
-    env['base.language.install'].create({'lang': 'es_AR', 'overwrite': True}).lang_install()
+    # # recargamos traducciones (por ahora no lo hacemos, si llegamos a ver que suma y es necesario probamos activarlo)
+    # env['base.language.install'].create({'lang': 'es_AR', 'overwrite': True}).lang_install()
