6727fd48527f2940d9bdf9c5399874df6ee4d86f
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-08-08 15:31:12 -0300

    [FIX] add patch of dni validation

--- l10n_latam_base/0.0.0/end-migration.py
+++ l10n_latam_base/0.0.0/end-migration.py
@@ -4,6 +4,10 @@ from openupgradelib import openupgrade, openupgrade_merge_records
 @openupgrade.migrate()
 def migrate(env, version):
     # sincronizacion de commercial fields
-    companies = env['res.partner'].search([('child_ids', '!=', False), '|', ("parent_id", "=", False), ("is_company", "=", True)])
+    # patch the dni validation para que no de error
+    from odoo.addons.l10n_ar.models import res_partner
+    res_partner.ResPartner.l10n_ar_identification_validation = lambda x: True
+    companies = env['res.partner'].search(
+        [('child_ids', '!=', False), '|', ("parent_id", "=", False), ("is_company", "=", True)])
     for company in companies:
         company._commercial_sync_to_children()
