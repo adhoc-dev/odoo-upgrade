0f45f00e026d7d5e5376fb52e31afcbb01739265
Juan José Scarafía <jjs@adhoc.com.ar>
2022-12-24 17:16:42 -0300

    [FIX] stock_analytic_tag as a merge not rename
    como el modulo stock_analytic ya existe indexado, da error si hacemos rename

--- base/16.0.0.0/dynamic_data.py
+++ base/16.0.0.0/dynamic_data.py
@@ -1,12 +1,12 @@
 renamed_modules = {
   'mass_editing': 'server_action_mass_edit',
-  'stock_analytic_tag': 'stock_analytic',
   'account_statement_import': 'account_statement_import_file',
   "knowledge": "document_knowledge",
 }
 
 
 merged_modules = {
+  'stock_analytic_tag': 'stock_analytic',
 }
 
 
