70a25b43b4a5bd7e6b99706ff6ec3b1b3582f43f
Juan José Scarafía <jjs@adhoc.com.ar>
2022-12-24 15:48:01 -0300

    [IMP] add some modules renames

--- base/16.0.0.0/dynamic_data.py
+++ base/16.0.0.0/dynamic_data.py
@@ -1,4 +1,8 @@
 renamed_modules = {
+  'mass_editing': 'server_action_mass_edit',
+  'stock_analytic_tag': 'stock_analytic',
+  'account_statement_import': 'account_statement_import_file',
+  "knowledge": "document_knowledge",
 }
 
 
