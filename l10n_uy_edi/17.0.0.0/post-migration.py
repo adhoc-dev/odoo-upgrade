c9fff99d7656b1f3e5ce3cdad31fb085ef2dba52
Maximiliano Mezzavilla <mem@adhoc.com.ar>
2024-09-17 15:36:31 -0300

    Testeo de querys adenda y limpieza de codigo

--- l10n_uy_edi/17.0.0.0/post-migration.py
+++ l10n_uy_edi/17.0.0.0/post-migration.py
@@ -5,7 +5,6 @@ _logger = logging.getLogger(__name__)
 
 @openupgrade.migrate()
 def migrate(env, version):
-
     # Popular nueva tabla con datos en el account move
     openupgrade.logged_query(env.cr, """
     INSERT INTO l10n_uy_edi_document (move_id, state, uuid, message, request_datetime)
@@ -34,11 +33,6 @@ def migrate(env, version):
       if datas:
            rec.attachment_file = datas
 
-    # l10n_uy_cfe_file_bu as attachment_id,
-    # TODO
-    # attachment_file → campo binary. no existe ¿Cómo popular?
-    # request_datetime (campo requerido, dejar valor por defecto para los viejos?)
-
     # Agregar relacion entre tabla edi document y move. campo 'l10n_uy_edi_document_id'
     openupgrade.logged_query(env.cr, """
         UPDATE account_move move
@@ -48,7 +42,7 @@ def migrate(env, version):
         WHERE edi.move_id == move.id
     """)
 
-    #Actualizamos los select de los estados del cfe
+    # Actualizamos los select de los estados del cfe
     openupgrade.logged_query(env.cr, """
         UPDATE account_move
         SET
@@ -63,7 +57,7 @@ def migrate(env, version):
         WHERE l10n_uy_cfe_state_bu IN ('not_apply', 'draft_cfe');
     """)
 
-    #Cambiamos los type de addendas
+    # Cambios en las addendas
     openupgrade.logged_query(env.cr, """
         UPDATE l10n_uy_edi_addenda
         SET
@@ -92,19 +86,12 @@ def migrate(env, version):
         WHERE type = 'adenda'
     """)
 
-    # convertimos nuestro "l10n_uy_cfe_file_bu" m2o a ir.attachment a un campo binary "attachment_file"
-    for move in electronic_move_with_l10n_uy_cfe_file_bu:
-        move.l10n_uy_cfe_file_bu.write({
-            'res_id': move.l10n_uy_edi_document_id.id,
-            # lo del name podria ser evitable
-            'name': move.l10n_uy_edi_document_id._get_xml_attachment_name(),
-            'res_model': 'l10n_uy_edi.document',
-            'res_field': 'attachment_file',
-        })
-
-    env['res.company'].search([('l10n_uy_edi_ucfe_env', '=', False)]).l10n_uy_edi_ucfe_env = 'demo'
     env['l10n_uy_edi.addenda'].search([('content', 'like', '{%}')]).is_legend = True
 
+    # Seteamos los ambientes
+    env['res.company'].search([('l10n_uy_edi_ucfe_env', '=', False)]).l10n_uy_edi_ucfe_env = 'demo'
+
+
+    # Los re-creamos
     env.ref('l10n_uy_edi.ir_cron_get_ucfe_notif').unlink()
-    # lo re-creamos
     env.ref('l10n_uy_edi.ir_cron_get_vendor_bills_received').unlink()
