03907ae81cb6b37e89b3b376a7aeb380a7e6078e
Pablo Montenegro <pam@adhoc.com.ar>
2025-06-03 11:20:06 -0300

    [FIX] l10n_ar_withholding: impuestos con secuencia de otra compañía Ticket: 92618

--- l10n_ar_withholding/17.0.0.0/pre-migration.py
+++ l10n_ar_withholding/17.0.0.0/pre-migration.py
@@ -22,3 +22,21 @@ def migrate(cr, version):
     env = api.Environment(cr, SUPERUSER_ID, {})
     openupgrade.copy_columns(cr, _column_copy)
     openupgrade.rename_fields(env, _field_renames)
+
+    # Hay casos de impuestos que pertenecen a una compañía pero la secuencia de dicho impuesto
+    # (l10n_ar_withholding_sequence_id) pertenece a otra compañía.
+    # Lo que hacemos en esta query es establecerle a la secuencia la misma compañía que la compañía del impuesto.
+    openupgrade.logged_query(
+        env.cr,
+            """
+            UPDATE ir_sequence SET company_id = atax.company_id
+            FROM account_tax atax
+            WHERE atax.l10n_ar_withholding_sequence_id = ir_sequence.id AND atax.company_id IN
+                (SELECT rc.id FROM res_company AS rc
+                JOIN res_partner AS rp ON rc.partner_id = rp.id
+                JOIN res_country AS c ON rp.country_id = c.id
+                WHERE c.code = 'AR')
+            AND atax.company_id != ir_sequence.company_id
+            AND ir_sequence.company_id IS NOT NULL;
+            """
+    )
