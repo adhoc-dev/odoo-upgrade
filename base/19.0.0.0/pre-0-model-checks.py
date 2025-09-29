d9d386e7c16d5a259fbcdfc046583f8f1ec3e8e6
Nicolas Mac Rouillon <nicolasmacr@gmail.com>
2025-09-29 10:37:46 +0000

    [ADD] base: init script for 19.0

---
+++ base/19.0.0.0/pre-0-model-checks.py
@@ -0,0 +1,25 @@
+from odoo.addons.base.models.ir_model import (
+    IrModelSelection,
+)
+
+
+_original_method = IrModelSelection._process_ondelete
+
+
+def _process_ondelete(self):
+    """Don't break on missing models when deleting their selection fields"""
+    to_process = self.browse([])
+    for selection in self:
+        try:
+            self.env[selection.field_id.model]  # pylint: disable=pointless-statement
+            to_process += selection
+        except KeyError:
+            continue
+    return _original_method(to_process)
+
+
+IrModelSelection._process_ondelete = _process_ondelete
+
+
+def migrate(cr, version):
+    pass
