5f8bb1bb7cfaae0f0cf7e9f35dbc6d0b68b18913
Nicolas Mac Rouillon <nicolasmacr@gmail.com>
2025-10-16 16:40:20 +0000

    [FIX] base: use correct class name for view check override

--- base/19.0.0.0/pre-0-disable-check-views.py
+++ base/19.0.0.0/pre-0-disable-check-views.py
@@ -1,20 +1,22 @@
-from odoo.addons.base.models.ir_ui_view import View
 import logging
+
+from odoo.addons.base.models.ir_ui_view import IrUiView
+
 _logger = logging.getLogger(__name__)
 
 
-_original_check_xml = View._check_xml
+_original_check_xml = IrUiView._check_xml
 
 
 def _check_xml(self):
-    """ Patch check_xml to avoid any error when loading views, we check them later """
+    """Patch check_xml to avoid any error when loading views, we check them later"""
     try:
         _original_check_xml
     except Exception as e:
-        _logger.warning('Invalid view definition. This is what we get:\n%s', e)
+        _logger.warning("Invalid view definition. This is what we get:\n%s", e)
 
 
-View._check_xml = _check_xml
+IrUiView._check_xml = _check_xml
 
 
 def migrate(cr, version):
