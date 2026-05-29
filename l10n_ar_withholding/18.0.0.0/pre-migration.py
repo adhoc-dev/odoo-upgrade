7dd969969fb5c29bb24d154225fa7a7593b01a21
Pablo Montenegro <pam@adhoc.com.ar>
2026-05-29 15:53:27 -0300

    [IMP] l10n_ar_withholding: pre-migration script renombrando campos
    withholding_non_taxable_minimum --> l10n_ar_payment_minimum_threshold de account.tax
    withholding_non_taxable_amount --> l10n_ar_base_minimum_threshold de account.tax
    
    closes ingadhoc/odoo-upgrade#134
    
    Signed-off-by: Victoria Halac - hav (#l10n) <hav@adhoc.inc>

--- l10n_ar_withholding/18.0.0.0/pre-migration.py
+++ l10n_ar_withholding/18.0.0.0/pre-migration.py
@@ -4,3 +4,7 @@ from odoo.upgrade import util
 def migrate(cr, version):
     if util.column_exists(cr, 'account_tax', 'codigo_regimen'):
         util.rename_field(cr, 'account.tax', 'codigo_regimen', 'l10n_ar_code')
+    if util.column_exists(cr, 'account_tax', 'withholding_non_taxable_minimum'):
+        util.rename_field(cr, 'account.tax', 'withholding_non_taxable_minimum', 'l10n_ar_payment_minimum_threshold')
+    if util.column_exists(cr, 'account_tax', 'withholding_non_taxable_amount'):
+        util.rename_field(cr, 'account.tax', 'withholding_non_taxable_amount', 'l10n_ar_base_minimum_threshold')
