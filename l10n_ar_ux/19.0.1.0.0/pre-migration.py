f92df353663c8008c712099f8fb6f6d4578f62dd
Martin Quinteros <maq@adhoc.com.ar>
2026-06-05 09:09:06 -0300

    [FIX] Add pre migration for maintan account used tags This commit removes unnecessary tags, but it should not do so in cases where they have already been used. In those cases, I only remove the external ID.

--- l10n_ar_ux/19.0.1.0.0/pre-migration.py
+++ l10n_ar_ux/19.0.1.0.0/pre-migration.py
@@ -17,6 +17,15 @@ _column_copy = {
     ],
 }
 
+def _xmlid_lookup(cr, xmlid: str) -> tuple[str, int]:
+    """copy off Low level xmlid lookup
+    Return (res_model, res_id)
+    """
+    module, name = xmlid.split('.', 1)
+    query = "SELECT model, res_id FROM ir_model_data WHERE module=%s AND name=%s"
+    cr.execute(query, [module, name])
+    result = cr.fetchone()
+    return result
 
 def migrate(cr, version):
     """Hacemos back up de los datos de actividad antiguos y preparamos para la carga de nuevos datos. """
@@ -29,5 +38,47 @@ def migrate(cr, version):
     # Copiar columnas viejas a columnas de backup en tablas referenciadas
     _logger.info("Copiando columnas de las columnas viejas a las columnas de respaldo")
     openupgrade.copy_columns(cr, _column_copy)
+
+
+    xml_id_names = [
+        "tag_a_cuenta_ganancias",
+        "tag_a_cuenta_iva",
+        "tag_iva_primer_parrafo",
+        "tag_unaffected_earnings",
+        "tag_impuestos_a_las_ganancias",
+        "tag_liquidacion_de_iva",
+        "tag_liquidacion_de_iibb",
+        "tag_liquidacion_de_ganancias",
+        "tag_liquidacion_sicore_aplicado",
+        "tag_liquidacion_iibb_aplicado",
+        "tax_tag_a_cuenta_suss",
+        "tax_tag_a_cuenta_iibb",
+        "tax_tag_a_cuenta_ganancias",
+        "tax_tag_a_cuenta_iva",
+        "tag_ret_perc_iibb_aplicada",
+        "tag_ret_perc_sicore_aplicada",
+    ]
+    for xml_id_name in xml_id_names:
+        account_tag_id = _xmlid_lookup(cr, f"l10n_ar_ux.{xml_id_name}")
+        if account_tag_id:
+            cr.execute(
+                """
+                SELECT 1
+                FROM account_account_tag_account_tax_repartition_line_rel
+                WHERE account_account_tag_id = %s
+                LIMIT 1
+            """,
+                (account_tag_id[1],),
+            )
+            used_in_taxes = cr.fetchone()
+            if used_in_taxes:
+                _logger.info(f"Eliminamos el extenal ref l10n_ar_ux.{xml_id_name} ya que se encuentra en uso")
+                cr.execute(
+                    """
+                    DELETE FROM ir_model_data
+                    WHERE module = 'l10n_ar_ux' AND name = %s
+                """,
+                    (xml_id_name,),
+                )
 
     _logger.info("Pre-migración completada exitosamente")
