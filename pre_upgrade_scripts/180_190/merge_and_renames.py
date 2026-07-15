36811192492abf0c5ebb14817e246c3f2ded9c07
Nicolas Mac Rouillon <nicolasmacr@gmail.com>
2026-07-15 15:40:43 +0000

    [ADD] Add pre-upgrade script for Odoo 18.0 to 19.0

--- pre_upgrade_scripts/180_190/merge_and_renames.py
+++ pre_upgrade_scripts/180_190/merge_and_renames.py
@@ -13,6 +13,7 @@ MERGE_MODULES = [
     ("sale_three_discounts", "sale_triple_discount"),
     ("l10n_ar_stock_custom", "l10n_ar_stock"),
     ("l10n_ar_stock_adhoc", "l10n_ar_stock"),
+    ("payment_redsys_oca", "payment_redsys"),
 ]
 RENAMED_MODULES = []
 RENAMED_XMLIDS = []
