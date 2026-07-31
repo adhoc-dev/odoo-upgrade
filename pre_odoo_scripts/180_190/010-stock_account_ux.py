a95c251e5221c4844b1a9201fef58912a3789d05
Santiago Co <cos@adhoc.inc>
2026-07-31 14:24:40 +0000

    [ADD] pre_odoo_scripts: backup valuation account move before Odoo upgrade
    New folder for scripts that run on the original database before sending it to
    upgrade.odoo.com, the git equivalent of the 2_pre "Pre Odoo" upgrade lines.
    Split by upgrade jump (180_190) plus an always folder, mirroring
    pre_upgrade_scripts, file name prefixed with the upgrade line sequence.
    
    First script replicates upgrade line 2456: stock_valuation_layer is dropped
    during the core upgrade, so the stock.move -> account.move map is saved into
    stock_move_account_move_id_bu, which the stock_account_ux 19.0.1.1.0
    post-migration consumes on the other side of the upgrade.

---
+++ pre_odoo_scripts/180_190/010-stock_account_ux.py
@@ -0,0 +1,24 @@
+BACKUP_TABLE = "stock_move_account_move_id_bu"
+
+
+def migrate(cr, version):
+    cr.execute("SELECT 1 FROM ir_module_module WHERE name = 'stock_account' AND state = 'installed'")
+    if not cr.fetchone():
+        return
+
+    cr.execute("DROP TABLE IF EXISTS %s" % BACKUP_TABLE)
+    cr.execute(
+        """
+        CREATE TABLE %s AS
+            SELECT DISTINCT ON (svl.stock_move_id)
+                   svl.stock_move_id AS stock_move_id,
+                   svl.account_move_id AS account_move_id
+              FROM stock_valuation_layer svl
+              JOIN account_move am ON am.id = svl.account_move_id
+             WHERE svl.account_move_id IS NOT NULL
+               AND svl.stock_move_id IS NOT NULL
+               AND am.state = 'posted'
+             ORDER BY svl.stock_move_id, svl.id
+        """
+        % BACKUP_TABLE
+    )
